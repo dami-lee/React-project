3fa5285f82edfec4db681c020188157dcc1e78ab
dami-lee <dami223@daum.net>
2019-11-18 16:23:13 +0900

    Add like Button and add style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import faker from 'faker';
 import FeedBox from './Components/FeedBox';
 import Profile from './Components/Profile';
 
+import './App.css';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,21 +16,27 @@ class App extends React.Component {
           image: faker.image.avatar(),
           name: '김영하',
           time: '2019년 11월 17일',
-          content: '<여행의 이유> 저자입니다.'
+          content: '<여행의 이유> 저자입니다.',
+          like: 1,
+          likeDone: true
         },
         {
           id: 1,
           image: faker.image.avatar(),
           name: '최은영',
           time: '2019년 11월 17일',
-          content: '<백의 그림자> 저자입니다.'
+          content: '<백의 그림자> 저자입니다.',
+          like: 0,
+          likeDone: false
         },
         {
           id: 2,
           image: faker.image.avatar(),
           name: '구병모',
           time: '2019년 11월 17일',
-          content: '<파과> 저자입니다.'
+          content: '<파과> 저자입니다.',
+          like: 0,
+          likeDone: false
         }
       ]
     }
@@ -37,7 +45,9 @@ class App extends React.Component {
   renderFeedBox = feedObj => {
     return (
       <FeedBox
-        key={feedObj.id}>
+        key={feedObj.id}
+        like={feedObj.like}
+        likeDone={feedObj.likeDone}>
         <Profile
           name={feedObj.name}
           image={feedObj.image}
@@ -54,7 +64,7 @@ class App extends React.Component {
     } = this;
 
     return (
-      <div>
+      <div class="feed">
         {
           feeds.map(feedObj =>
             renderFeedBox(feedObj)
