3112584c50cef6a7836298ed955bf08045d93af2
dami-lee <dami223@daum.net>
2019-11-19 23:54:39 +0900

    Transform functional component  into class type

--- src/Components/feed/feedWrapper.js
+++ src/Components/feed/feedWrapper.js
@@ -1,40 +1,48 @@
-import React, { useState } from 'react';
+import React from 'react';
 import faker from 'faker';
 import FeedBox from './FeedBox';
 import FeedProfile from './FeedProfile';
 
-const FeedWrapper = () => {
-    const [feeds, setFeeds] = useState([
-        {
-            id: 0,
-            image: faker.image.avatar(),
-            name: '김영하',
-            time: '2019년 11월 17일',
-            content: '<여행의 이유> 저자입니다.',
-            like: 1,
-            likeDone: true
-        },
-        {
-            id: 1,
-            image: faker.image.avatar(),
-            name: '최은영',
-            time: '2019년 11월 17일',
-            content: '<백의 그림자> 저자입니다.',
-            like: 0,
-            likeDone: false
-        },
-        {
-            id: 2,
-            image: faker.image.avatar(),
-            name: '구병모',
-            time: '2019년 11월 17일',
-            content: '<파과> 저자입니다.',
-            like: 0,
-            likeDone: false
-        }
-    ]);
+import '../../assets/stylesheets/feed/FeedWrapper.css';
 
-    const renderFeedBox = feedObj => {
+class FeedWrapper extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            feeds: [
+                {
+                    id: 0,
+                    image: faker.image.avatar(),
+                    name: '김영하',
+                    time: '2019년 11월 17일',
+                    content: '<여행의 이유> 저자입니다.',
+                    like: 1,
+                    likeDone: true
+                },
+                {
+                    id: 1,
+                    image: faker.image.avatar(),
+                    name: '최은영',
+                    time: '2019년 11월 17일',
+                    content: '<백의 그림자> 저자입니다.',
+                    like: 0,
+                    likeDone: false
+                },
+                {
+                    id: 2,
+                    image: faker.image.avatar(),
+                    name: '구병모',
+                    time: '2019년 11월 17일',
+                    content: '<파과> 저자입니다.',
+                    like: 0,
+                    likeDone: false
+                }
+            ]
+        }
+    }
+
+    renderFeedBox = feedObj => {
       return (
         <FeedBox
             key={feedObj.id}
@@ -48,16 +56,20 @@ const FeedWrapper = () => {
         </FeedBox>
       )
     }
+    
+    render() {
+        const { feeds } = this.state;
 
-    return (
-        <div className="FeedWrapper">
-            {
-                feeds.map(feedObj => (
-                    renderFeedBox(feedObj)
-                ))
-            }
-        </div>
-    );
+        return (
+            <div className="feedWrapper">
+                {
+                    feeds.map(feedObj => (
+                        this.renderFeedBox(feedObj)
+                    ))
+                }
+            </div>
+        );
+    }
 }
 
 export default FeedWrapper;
