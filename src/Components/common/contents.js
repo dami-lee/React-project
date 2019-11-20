d6d74cbd6bcd42146f509ccb0fd28d5f7c349374
dami-lee <dami223@daum.net>
2019-11-20 23:15:11 +0900

    [component] Add user profile component

--- src/Components/common/contents.js
+++ src/Components/common/contents.js
@@ -4,15 +4,17 @@ import ProfileWrapper from '../profile/ProfileWrapper';
 import WidgetWrapper from '../widget/widgetWrapper';
 import SignUpWrapper from '../signup/signupWrapper';
 
+import '../../assets/stylesheets/common/contents.css';
+
 const Contents = () => {
     return (
         <div>
             <div>
                 <SignUpWrapper />
             </div>
-            <div>
-                <FeedWrapper />
+            <div class="feed">
                 <ProfileWrapper />
+                <FeedWrapper />
                 <WidgetWrapper />
             </div>
         </div>
--- src/Components/profile/profileWrapper.js
+++ src/Components/profile/profileWrapper.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import faker from 'faker';
 
 import '../../assets/stylesheets/profile/ProfileWrapper.css';
 
 const ProfileWrapper = () => {
+    const userData = {
+        name: 'User Name',
+        image: faker.image.avatar(),
+        userId: 'user_id'
+    }
+
     return (
-        <></>
+        <div className="profileWrapper">
+            <div class="profileImage">
+                <img src={userData.image} alt={userData.name} />
+            </div>
+            <div class="profileName">
+                {userData.name}
+            </div>
+            <div class="profileId">
+                @{userData.userId}
+            </div>
+        </div>
     )
 }
 
